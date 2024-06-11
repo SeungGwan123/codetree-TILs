af5242f546c615aa155dbed4a697bdcc7adb5dac
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2024-06-11 13:57:59 +0900

    Title: 삼각형 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240611/삼각형 컨베이어 벨트/conveyor-belt-triangle.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;int t;
+    cin>>n>>t;
+    vector<int> v[3];
+    vector<int> temp_v[3];
+    int row = 0;
+    for(int i=0;i<n*3;i++){
+        int temp;
+        cin>>temp;
+        v[row].push_back(temp);
+        temp_v[row].push_back(temp);
+        if(i%n==n-1) row++;
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<n;j++){
+            int now = v[i][j];
+            int time = j+t;
+            temp_v[time/n%3][time%n] = now;
+        }
+    }
+    for(auto iter : temp_v){
+        for(auto a : iter){
+            cout<<a<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
