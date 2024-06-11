6a271e7144da568a0283ba6847c79a932660efe8
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2024-06-11 13:38:08 +0900

    Title: 컨베이어 벨트, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240611/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;int t;
+    cin>>n>>t;
+    vector<vector<int>> v;
+    vector<int> one;
+    vector<int> two;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        one.push_back(temp);
+    }
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        two.push_back(temp);
+    }
+    v.push_back(one);
+    v.push_back(two);
+    vector<vector<int>> temp = v;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<n;j++){
+            int now = temp[i][j];
+            if(j+t>=n){
+                if(i==0) v[1][j+t-n] = now;
+                else v[0][j+t-n] = now;
+            }else v[i][j+t] = now;
+        }
+    }
+    for(int i=0;i<2;i++){
+        for(int j=0;j<n;j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
