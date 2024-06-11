dd4555c597c879b46249899ae7a6c6c2e522d543
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2024-06-11 13:17:24 +0900

    Title: 양수 직사각형의 최대 크기, Time: 219ms, Memory: 1MB, Status: Passed - CodeTree

--- 240611/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 240611/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -5,13 +5,18 @@ using namespace std;
 int n;int m;
 int board[20][20];
 priority_queue<int> pq;
-void big_square(vector<pair<int,int>> v,set<pair<int,int>> s){
+set<tuple<int,int,int,int>> total_s;
+void big_square(vector<pair<int,int>> v,set<pair<int,int>> s,int x,int y){
     vector<pair<int,int>> temp_v = v;
     set<pair<int,int>> temp_s = s;
     bool check = true;
+    int temp_x = x;
+    int temp_y = y;
     for(int i=0;i<v.size();i++){
         int a = v[i].first+1;
         int b = v[i].second;
+        temp_x = max(temp_x,a);
+        temp_y = max(temp_y,b);
         if(a>=n) {
             check = false;
             break;
@@ -24,16 +29,21 @@ void big_square(vector<pair<int,int>> v,set<pair<int,int>> s){
         temp_v.push_back({a,b});
         temp_s.insert({a,b});
     }
-    if(check){
+    if(check&&total_s.count({x,y,temp_x,temp_y})==0){
+        total_s.insert({x,y,temp_x,temp_y});
         pq.push(temp_v.size());
-        big_square(temp_v,temp_s);
+        big_square(temp_v,temp_s,x,y);
     }
     vector<pair<int,int>> t_v = v;
     set<pair<int,int>> t_s = s;
     check = true;
+    temp_x = x;
+    temp_y = y;
     for(int i=0;i<v.size();i++){
         int a = v[i].first;
         int b = v[i].second+1;
+        temp_x = max(temp_x,a);
+        temp_y = max(temp_y,b);
         if(b>=m) {
             check = false;
             break;
@@ -46,9 +56,10 @@ void big_square(vector<pair<int,int>> v,set<pair<int,int>> s){
         t_v.push_back({a,b});
         t_s.insert({a,b});
     }
-    if(check){
+    if(check&&total_s.count({x,y,temp_x,temp_y})==0){
+        total_s.insert({x,y,temp_x,temp_y});
         pq.push(t_v.size());
-        big_square(t_v,t_s);
+        big_square(t_v,t_s,x,y);
     }
 }
 int main() {
@@ -64,9 +75,10 @@ int main() {
         for(int j=0;j<m;j++){
             if(board[i][j]<=0)continue;
             pq.push(1);
+            total_s.insert({i,j,i,j});
             vector<pair<int,int>> v = {{i,j}};
             set<pair<int,int>> s = {{i,j}};
-            big_square(v,s);
+            big_square(v,s,i,j);
         }
     }
     if(pq.empty()) cout<<"-1";
