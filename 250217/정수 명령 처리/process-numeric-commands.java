c6a1366829a87b1b5e9dd7d96386272d0fee0750
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-02-17 10:05:44 +0900

    Title: 정수 명령 처리, Time: 365ms, Memory: 21MB, Status: Passed - CodeTree

--- 250217/정수 명령 처리/process-numeric-commands.java
+++ 250217/정수 명령 처리/process-numeric-commands.java
@@ -3,11 +3,29 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        Stack<Integer> stack = new Stack<>();
         int num = sc.nextInt();
+        sc.nextLine();
         for(int i=0;i<num;i++){
             String[] temp = sc.nextLine().split(" ");
-            System.out.print(temp)
+            if(temp.length>1)stack.push(Integer.parseInt(temp[1]));
+            else{
+                switch(temp[0]){
+                    case "pop":
+                        System.out.println(stack.pop());
+                        break;
+                    case "size":
+                        System.out.println(stack.size());
+                        break;
+                    case "empty":
+                        if(stack.isEmpty()) System.out.println("1");
+                        else System.out.println("0");
+                        break;
+                    case "top":
+                        System.out.println(stack.peek());
+                        break;
+                }
+            }
         }       
     }
 }
