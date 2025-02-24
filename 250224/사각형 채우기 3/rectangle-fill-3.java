d477d699b340815913305de305683612d2599dfa
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-02-24 14:27:50 +0900

    Title: 사각형 채우기 3, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250224/사각형 채우기 3/rectangle-fill-3.java
+++ 250224/사각형 채우기 3/rectangle-fill-3.java
@@ -1,15 +1,21 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int divide = 1000000007;
         List<Integer> list = new ArrayList<>();
         list.add(0);
         list.add(2);
         list.add(7);
         list.add(22);
         for(int i=4;i<=n;i++){
-            list.add((list.get(i-1)*2+list.get(i-2)*3+list.get(i-3)*2)%1000000007);
+            int one = list.get(i-1)*2%divide;
+            int two = list.get(i-2)*3%divide;
+            int three = list.get(i-3)*2%divide;
+            int now = (one+two+three)%divide;
+            list.add(now);
         }
         System.out.println(list.get(n));
     }
