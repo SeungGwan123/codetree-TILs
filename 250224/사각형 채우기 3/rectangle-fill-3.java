72f9e8e80d1bdfe5813131bd0a14f9c52e2a1377
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-02-24 14:35:28 +0900

    Title: 사각형 채우기 3, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250224/사각형 채우기 3/rectangle-fill-3.java
+++ 250224/사각형 채우기 3/rectangle-fill-3.java
@@ -13,14 +13,8 @@ public class Main {
         for(int i=4;i<=n;i++){
             int one = list.get(i-1)*2%divide;
             int two = list.get(i-2)*3%divide;
-            int three = 0;
-            int block = i;
-            while(block%2==1 && block>=3){
-                three += (list.get(i-block)*2%divide);
-                three %= divide;
-                block-=2;
-            }
-            int now = (one+two+three)%divide;
+            
+            int now = (one+two+2)%divide;
             list.add(now);
         }
         System.out.println(list.get(n));
