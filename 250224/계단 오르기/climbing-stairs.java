4fec1fcf1621151d39e407eda945b1f3787387d8
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-02-24 14:10:08 +0900

    Title: 계단 오르기, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250224/계단 오르기/climbing-stairs.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(0);
+        list.add(1);
+        list.add(1);
+        for(int i=4;i<=n;i++){
+            list.add(list.get(i-2)+list.get(i-3));
+        }
+        System.out.println(list.get(n));
+    }
+}
