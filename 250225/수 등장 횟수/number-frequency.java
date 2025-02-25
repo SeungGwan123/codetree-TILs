74982f559a721b6b093dfaa4b155b58cedcdf5ec
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-02-25 16:20:34 +0900

    Title: 수 등장 횟수, Time: 1587ms, Memory: 54MB, Status: Wrong Answer - CodeTree

---
+++ 250225/수 등장 횟수/number-frequency.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] queries = new int[m];
+        for (int i = 0; i < m; i++) {
+            queries[i] = sc.nextInt();
+        }
+        HashMap<Integer,Integer> hash = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            if(hash.containsKey(arr[i])){
+                hash.put(arr[i],hash.get(arr[i])+1);
+            }else{
+                hash.put(arr[i],1);
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(hash.containsKey(queries[i])){
+                System.out.print(hash.get(arr[i])+" ");
+            }else System.out.print(0+" ");
+        }
+    }
+}
