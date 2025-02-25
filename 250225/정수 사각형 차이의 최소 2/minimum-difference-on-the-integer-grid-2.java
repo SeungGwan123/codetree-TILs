746e26e53b1fea4e0a95d04a499a485556853c18
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-02-25 14:48:23 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250225/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] grid = new int[n][n];
+        int[][][] dp = new int[n][n][2]; // min max
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[0][0][0] = grid[0][0];
+                    dp[0][0][1] = grid[0][0];
+                }else if(i==0){
+                    dp[i][j][0] = Math.min(dp[i][j-1][0],grid[i][j]);
+                    dp[i][j][1] = Math.max(dp[i][j-1][1],grid[i][j]);
+                }else if(j==0){
+                    dp[i][j][0] = Math.min(dp[i-1][j][0],grid[i][j]);
+                    dp[i][j][1] = Math.max(dp[i-1][j][1],grid[i][j]);
+                }else{
+                    int now = grid[i][j];
+                    int[] left = new int[]{Math.min(dp[i][j-1][0],now),Math.max(dp[i][j-1][1],now)};
+                    int[] up = new int[]{Math.min(dp[i-1][j][0],now),Math.max(dp[i-1][j][1],now)};
+                    if(left[1]-left[0]<up[1]-up[0]){
+                        dp[i][j] = left;
+                    }else dp[i][j] = up;
+                }
+
+                for(int a=0;a<n;a++){
+                    for(int b=0;b<n;b++){
+                        System.out.print(dp[a][b][0]+" "+dp[a][b][1]+"  ");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+            }
+        }
+        System.out.println(dp[n-1][n-1][1]-dp[n-1][n-1][0]);
+    }
+}
