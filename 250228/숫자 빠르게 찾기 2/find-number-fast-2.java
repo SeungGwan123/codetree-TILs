e9d3bed7790c2c972a297d5357cbffe9e2b90c0f
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-02-28 08:20:24 +0900

    Title: 숫자 빠르게 찾기 2, Time: 1688ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 250228/숫자 빠르게 찾기 2/find-number-fast-2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        TreeSet<Integer> set = new TreeSet<>();
+        for(int i=0;i<n;i++){
+            set.add(sc.nextInt());
+        }
+        for(int i=0;i<m;i++){
+            Integer ceil = set.ceiling(sc.nextInt());
+            if(ceil==null) System.out.println("-1");
+            else System.out.println(ceil);
+        }
+    }
+}
