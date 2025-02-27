7e1f0d4883cba5b2dec4bfaf3d12166f106245d8
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-02-27 09:06:24 +0900

    Title: 자리 바꾸기 2, Time: 3041ms, Memory: 146MB, Status: Time Limit Exceed - CodeTree

--- 250227/자리 바꾸기 2/changing-seats-2.java
+++ 250227/자리 바꾸기 2/changing-seats-2.java
@@ -30,14 +30,27 @@ public class Main {
             seat[first] = seat[second];
             seat[second] = temp;
 
-            if(!set.contains(seat[first]+"-"+first)){
+            int first_size = set.size();
+            set.add(seat[first]+"-"+first);
+            if(first_size<set.size()){
                 set.add(seat[first]+"-"+first);
                 s_num[seat[first]]++;
             }
-            if(!set.contains(seat[second]+"-"+second)){
+
+            int second_size = set.size();
+            set.add(seat[second]+"-"+second);
+            if(second_size<set.size()){
                 set.add(seat[second]+"-"+second);
                 s_num[seat[second]]++;
             }
+            // if(!set.contains(seat[first]+"-"+first)){
+            //     set.add(seat[first]+"-"+first);
+            //     s_num[seat[first]]++;
+            // }
+            // if(!set.contains(seat[second]+"-"+second)){
+            //     set.add(seat[second]+"-"+second);
+            //     s_num[seat[second]]++;
+            // }
         }
         for(int i=0;i<n;i++){
             System.out.println(s_num[i]);
