641d0986db955a8a77c5d08c9b8b66e4d68dbdd5
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-04 08:49:38 +0900

    Title: 최대로 연속한 숫자, Time: 149ms, Memory: 13MB, Status: Runtime Error - CodeTree

--- 250304/최대로 연속한 숫자/maximum-consecutive-number.java
+++ 250304/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -5,29 +5,33 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int[] removals = new int[m];
+        int[] removals = new int[n+1];
         TreeSet<Integer> set = new TreeSet<>();
         TreeSet<Integer> seq = new TreeSet<>();
         for (int i = 0; i < m; i++) {
             int num = sc.nextInt();
-            
-             
             
             Integer high = set.higher(num);
             Integer low = set.lower(num);
             if(high==null&&low==null){
                 seq.add(n-num);
                 seq.add(num);
+                removals[n-num]++;
+                removals[num]++;
             }else{
 
-            if(high==null) high = n-num;
-            else high -= (num+1);
+                if(high==null) high = n-num;
+                else high -= (num+1);
 
-            if(low==null) low = num;
-            else low = num-low-1;
-            seq.remove(high+low+1);
-            seq.add(high);
-            seq.add(low);
+                if(low==null) low = num;
+                else low = num-low-1;
+                removals[high+low+1]--;
+                if(removals[high+low+1]==0) seq.remove(high+low+1);
+                seq.add(high);
+                seq.add(low);
+
+                removals[high]++;
+                removals[low]++;
             }
             set.add(num);
             System.out.println(seq.last());
