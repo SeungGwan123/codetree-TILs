38a86058ec28b82976cd0cf8fea57ab2c0e7a8ad
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-05 12:32:25 +0900

    Title: K번째로 작은 쌍의 합, Time: 3072ms, Memory: 112MB, Status: Time Limit Exceed - CodeTree

---
+++ 250305/K번째로 작은 쌍의 합/sum-of-kth-smallest-pair.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class num implements Comparable<num>{
+    PriorityQueue<Integer> pq;
+    int x;
+    public num(int x,PriorityQueue<Integer> pq){
+        this.x = x;
+        this.pq = pq;
+    }
+
+    @Override
+    public int compareTo(num n){
+        Integer pq_top = this.pq.peek();
+        Integer n_top = n.pq.peek();
+        if(pq_top==null) pq_top=0;
+        if(n_top==null) n_top=0;
+        return (pq_top+this.x) - (n_top+n.x);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[m];
+        PriorityQueue<Integer> temp_pq = new PriorityQueue<>();
+        PriorityQueue<num> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++)
+            temp_pq.add(sc.nextInt());
+        for (int i = 0; i < m; i++){
+            PriorityQueue<Integer> newpq = new PriorityQueue<>(temp_pq);
+            pq.add(new num(sc.nextInt(),newpq));
+        }
+        for(int i=0;i<k-1;i++){
+            num now = pq.peek();
+            //System.out.println(now.pq.peek()+" "+now.x);
+            if(now.pq.size()==1) pq.poll();
+            else {
+                pq.poll();
+                now.pq.poll();
+                pq.add(now);
+            }
+        }
+
+        System.out.println(pq.peek().pq.peek()+pq.peek().x);
+    }
+}
