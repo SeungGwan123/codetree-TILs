0afc61c7cf9a0a52f182c6bb4390a33536267db3
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-07 08:52:45 +0900

    Title: 마라톤 중간에 택시타기, Time: 966ms, Memory: 51MB, Status: Passed - CodeTree

--- 250307/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.java
+++ 250307/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.java
@@ -12,9 +12,20 @@ public class Main {
         int[] left = new int[n];
         int[] right = new int[n];
         for(int i=1;i<n;i++){
-            left[i] = Math.abs(x[i]-x[i-1]) + Math.abs(y[i]-y[i-1]);
-            right[n-1-i] = Math.abs(x[n-1-i]-x[n-1-(i-1)]) + Math.abs(y[n-1-i]-y[n-1-(i-1)]);
+            left[i] = Math.abs(x[i]-x[i-1]) + Math.abs(y[i]-y[i-1]) + left[i-1];
+            right[n-1-i] = Math.abs(x[n-1-i]-x[n-1-(i-1)]) + Math.abs(y[n-1-i]-y[n-1-(i-1)]) + right[n-1-(i-1)];
         }
+
+        // for(int i=0;i<n;i++){
+        //     System.out.print(left[i]+" ");
+        // }
+        // System.out.println();
+
+        // for(int i=0;i<n;i++){
+        //     System.out.print(right[i]+" ");
+        // }
+        // System.out.println();
+
         long result = 1000000000;
         for(int i=1;i<n-1;i++){
             int jump = Math.abs(x[i-1]-x[i+1]) + Math.abs(y[i-1]-y[i+1]);
