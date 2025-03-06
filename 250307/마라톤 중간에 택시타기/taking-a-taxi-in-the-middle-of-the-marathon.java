a3e6e86c8607912c5907c1ee6ccd9ed8cd25983f
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-07 08:43:27 +0900

    Title: 마라톤 중간에 택시타기, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250307/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+        int[] left = new int[n];
+        int[] right = new int[n];
+        for(int i=1;i<n;i++){
+            left[i] = Math.abs(x[i]-x[i-1]) + Math.abs(y[i]-y[i-1]);
+            right[n-1-i] = Math.abs(x[n-1-i]-x[n-1-(i-1)]) + Math.abs(y[n-1-i]-y[n-1-(i-1)]);
+        }
+        long result = 1000000000;
+        for(int i=1;i<n-1;i++){
+            int jump = Math.abs(x[i-1]-x[i+1]) + Math.abs(y[i-1]-y[i+1]);
+            result = Math.min(left[i-1]+right[i+1]+jump,result);
+        }
+        System.out.println(result);
+    }
+}
