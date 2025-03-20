a5b94196131a3d8475cdc088c6af96bfc63cdc2b
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-21 08:21:53 +0900

    Title: 정수 분배하기, Time: 129ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250321/정수 분배하기/distributing-integers.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int low = 0;
+        int high = 100000;
+        int mid = (high+low)/2;
+        int result = 0;
+        while(low<=high){
+            int count = 0;
+            for(int i=0;i<n;i++){
+                count+=(arr[i]/mid);
+                if(count>=m) break;
+            }
+            if(count>=m){
+                result = mid;
+                low = mid+1;
+            }else high = mid-1;
+
+            mid = (high+low)/2;
+        }
+        System.out.println(result);
+    }
+}
