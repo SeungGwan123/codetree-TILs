d62c425ed911a971e3641ad75cc8b8e816260678
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-20 16:29:30 +0900

    Title: 자연수 n개의 합, Time: 3054ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 250320/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 250320/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -5,10 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         long s = sc.nextLong();
         long answer = 0;
-        long high = 100000000000;
+        long high = 2000000000L;
         long mid = high/2;
         long low = 0;
-        while(low<high){
+        while(low<=high){
             long result = 0;
             for(int i=0;i<=mid;i++){
                 result+=i;
