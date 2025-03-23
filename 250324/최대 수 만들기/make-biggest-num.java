b620d88ec67322ac225914788b97ba322332728a
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-24 08:48:20 +0900

    Title: 최대 수 만들기, Time: 734ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 250324/최대 수 만들기/make-biggest-num.java
+++ 250324/최대 수 만들기/make-biggest-num.java
@@ -11,7 +11,7 @@ public class Main {
         Arrays.sort(arr, new Comparator<String>() {
             @Override
             public int compare(String a,String b){
-                return Long.compare(Long.parseLong(b + a), Long.parseLong(a + b));
+                return (b+a).compareTo(a+b);
             }
         });
         String result = "";
