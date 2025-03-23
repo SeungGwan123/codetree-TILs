d8c7b195861b94b53bb10a434a042a3ce99bc41d
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-24 08:44:36 +0900

    Title: 최대 수 만들기, Time: 247ms, Memory: 22MB, Status: Runtime Error - CodeTree

---
+++ 250324/최대 수 만들기/make-biggest-num.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLine();
+        }
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String a,String b){
+                return Integer.parseInt(b+a) - Integer.parseInt(a+b);
+            }
+        });
+        String result = "";
+        for(int i=0;i<arr.length;i++){
+            result+=arr[i];
+        }
+        System.out.println(result);
+    }
+}
