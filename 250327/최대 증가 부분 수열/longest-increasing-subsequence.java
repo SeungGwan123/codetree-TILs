8819373434815e62238f883d407d7fe57dff84ac
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-27 08:42:14 +0900

    Title: 최대 증가 부분 수열, Time: 190ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 250327/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        int num = list.size()-1;
+        for(int i=1;i<n;i++){
+            int now = arr[i];
+            if(list.get(num)<now){
+                list.add(now);
+                num++;
+            }else{
+                int low = 0;
+                int high = num;
+                int result = 0;
+                while(low<=high){
+                    int mid = (low+high)/2;
+                    if(list.get(mid)>=now){
+                        low = mid+1;
+                        result = mid;
+                    }else high = mid - 1;
+                }
+                list.set(result,now);
+            }
+        }
+        System.out.println(list.size());
+    }
+}
