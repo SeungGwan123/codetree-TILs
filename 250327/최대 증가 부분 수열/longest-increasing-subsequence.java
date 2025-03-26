1fcc11cdb3ad5b3553fdd333afca6a7f14bc72c0
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-03-27 08:53:32 +0900

    Title: 최대 증가 부분 수열, Time: 199ms, Memory: 12MB, Status: Wrong Answer - Codetree

--- 250327/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 250327/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -22,8 +22,10 @@ public class Main {
                     int mid = (low+high)/2;
                     if(list.get(mid)>=now){
                         low = mid+1;
+                    }else {
+                        high = mid - 1;
                         result = mid;
-                    }else high = mid - 1;
+                    }
                 }
                 list.set(result,now);
             }
