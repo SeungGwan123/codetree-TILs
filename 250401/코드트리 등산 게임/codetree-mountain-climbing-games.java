e3602974caf33f2b7143d37e8f1d1f707ee43c64
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-04-01 15:48:35 +0900

    Title: 코드트리 등산 게임, Time: 114ms, Memory: 13MB, Status: Wrong Answer - Codetree

--- 250401/코드트리 등산 게임/codetree-mountain-climbing-games.java
+++ 250401/코드트리 등산 게임/codetree-mountain-climbing-games.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
     public static List<Integer> height = new ArrayList<>();
     public static List<Integer> length = new ArrayList<>();
-
+    public static Stack<Integer>[] stack = new Stack[50001];
     public static List<Integer> makeLIS(List<Integer> list,int san){
         int last_san = list.get(list.size() - 1);
         if(last_san<san){
@@ -27,6 +27,7 @@ public class Main {
                     left = mid + 1;
                 }
             }
+            stack[result].push(list.get(result));
             list.set(result, san);
             length.add(result + 1);
             height.set(height.size() - 1,Math.max(max_height,height.get(height.size() - 1)));
@@ -38,8 +39,11 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         List<Integer> mountain = new ArrayList<>();
-        List<List<Integer>> lis = new ArrayList<>();
+        List<Integer> lis = new ArrayList<>();
         
+        for(int i=0;i<50001;i++){
+            stack[i] = new Stack<>();
+        }
         String[] initData = br.readLine().split(" ");
         for(int i=2;i<initData.length;i++){
             int san = Integer.parseInt(initData[i]);
@@ -47,15 +51,11 @@ public class Main {
             
             if(lis.size()==0){
                 height.add(san);
-                List<Integer> temp_lis = new ArrayList<>();
-                temp_lis.add(san);
+                lis.add(san);
                 length.add(1);
-                lis.add(temp_lis);
             }else{
                 height.add(height.get(height.size() - 1));
-                List<Integer> temp_lis = new ArrayList<>(lis.get(lis.size()-1));
-                temp_lis = makeLIS(temp_lis, san);
-                lis.add(temp_lis);
+                lis = makeLIS(lis, san);
             }
         }
 
@@ -65,20 +65,23 @@ public class Main {
                 int san = Integer.parseInt(command[1]);
                 mountain.add(san);
                 height.add(height.get(height.size() - 1));
-                List<Integer> temp_lis = new ArrayList<>(lis.get(lis.size()-1));
-                temp_lis = makeLIS(temp_lis, san);
-                lis.add(temp_lis);
+                lis = makeLIS(lis, san);
             }else if(command[0].equals("300")){
+                int san = mountain.get(mountain.size() - 1);
+                if(lis.get(lis.size() - 1)==san){
+                    lis.remove(lis.size() - 1);
+                    if(!stack[lis.size()].isEmpty()){
+                        lis.add(stack[lis.size()].pop());
+                    }
+                }
                 mountain.remove(mountain.size() - 1);
                 height.remove(height.size() - 1);
                 length.remove(length.size() - 1);
-                lis.remove(lis.size() - 1);
             }else{
                 int cable = Integer.parseInt(command[1]) - 1;
                 long result = length.get(cable) - 1;
-                List<Integer> last_lis = lis.get(lis.size() - 1);
 
-                result += last_lis.size();
+                result += lis.size();
                 System.out.println(1000000*result+height.get(height.size() - 1));
             }
         }
