d7b7162ce118459dfe5cbade5cf363e95bee6dca
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-04-01 22:44:56 +0900

    Title: 코드트리 등산 게임, Time: 140ms, Memory: 4MB, Status: Passed - Codetree

--- 250401/코드트리 등산 게임/codetree-mountain-climbing-games.cpp
+++ 250401/코드트리 등산 게임/codetree-mountain-climbing-games.cpp
@@ -1,139 +1,64 @@
-#include <vector>
 #include <iostream>
-#include <map>
-#include <set>
-#include <cstdio>
-
-#define add_points 1000000
+#include <vector>
 using namespace std;
 
-struct Mountain {
-    int height;
-    int idx;
-    int lis;
-    Mountain* prev;
-    Mountain(int h, int i) : height(h), idx(i), prev(NULL), lis(0) {};
-};
+int q;
+vector<int> index;
+vector<vector<int>> mount;
 
-struct set_cmp {
-    bool operator()(const Mountain* m1, const Mountain* m2) const {
-        if (m1->height == m2->height) return m1->idx < m2->idx;
-        return m1->height > m2->height;
-    }
-};
-
-int b_search(Mountain* m, vector<set<Mountain*, set_cmp>>& lis2mountain) {
-    if(lis2mountain.empty()) return 0;
-    int right = lis2mountain.size()-1;
-    int left = 0;
-    while(left<=right) {
-        int mid = (left+right)/2;
-        auto lis_min_value = lis2mountain[mid].end(); lis_min_value--;
-        int min_value = (*lis_min_value)->height;
-        if(min_value>=m->height) {
-            right = mid-1;
-        }
-        else {
-            left = mid+1;
+int search(int h) {
+    if(mount.size()==0)
+        return 0;
+    int s=0,e=mount.size()-1;
+    int idx=mount.size();
+    while(s<=e) {
+        int m=(s+e)/2;
+        if(h<=mount[m].back()) {
+            idx=m;
+            e=m-1;
+        } else {
+            s=m+1;
         }
     }
-    return left;
-}
-
-void addLIS(Mountain* m, vector<set<Mountain*, set_cmp>>& lis2mountain) {
-    int k = b_search(m, lis2mountain);
-    if(lis2mountain.size()==k) {
-        lis2mountain.emplace_back(set<Mountain*, set_cmp> {m});
-    }
-    else {
-        lis2mountain[k].insert(m);
-    }
-    if(k) {
-        auto it = lis2mountain[k-1].upper_bound(m);
-        m->prev = *(--it);
-        m->lis = k;
-        return;
-    }
-    else {
-        m->lis = 0;
-    }
-}
-
-void removeLIS(Mountain* m, vector<set<Mountain*, set_cmp>>& lis2mountain) {
-    int k = m->lis;
-    lis2mountain[k].erase(m);
-    if(lis2mountain[k].empty())
-        lis2mountain.pop_back();
-}
-
-void build_mountain(vector<Mountain*>& mountain, vector<set<Mountain*, set_cmp>>& lis2mountain, int h) {
-    int i = mountain.size();
-    Mountain* m = new Mountain(h, i);
-    addLIS(m, lis2mountain);
-    mountain.push_back(m);
-}
-
-void bigbang(vector<Mountain*>& mountain, vector<set<Mountain*, set_cmp>>& lis2mountain, int n) {
-    for(int i=0; i<n; i++) {
-        int h;
-        cin >> h;
-        build_mountain(mountain, lis2mountain, h);
-    }
-}
-
-void earthquake(vector<Mountain*>& mountain, vector<set<Mountain*, set_cmp>>& lis2mountain) {
-    Mountain* m = mountain[mountain.size()-1];
-    removeLIS(m, lis2mountain);
-    delete m;
-    mountain.pop_back();
-}
-
-long long simulation(vector<Mountain*>& mountain, vector<set<Mountain*, set_cmp>>& lis2mountain, int idx) {
-    long long score = 0;
-    Mountain* m = mountain[idx];
-    int lis = lis2mountain.size()-1;
-    int highest = (*lis2mountain[lis].begin())->height;
-    score += (m->lis + 1) * add_points;
-    score += lis * add_points;
-    score += highest;
-    return score;
+    return idx;
 }
 
 int main() {
-    int Q;
-    cin >> Q;
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-    vector<Mountain*> mountain;
-    vector<set<Mountain*, set_cmp>> lis2mountain;
-    for(int i=0; i<Q; i++) {
-        int cmd;
-        cin >> cmd;
-        switch (cmd) {
-        case 100:
-            if(i!=0) {
-                cout << "bigbang error" << endl;
-                return 0;
+    cin>>q;
+    while(q--) {
+        int o;cin>>o;
+        if(o==100){
+            int n;cin>>n;
+            for(int i=0;i<n;i++) {
+                int h;cin>>h;
+                int idx=search(h);
+                index.push_back(idx);
+                if(idx==mount.size())
+                    mount.push_back({});
+                mount[idx].push_back(h);
             }
-            int n;
-            cin >> n;
-            bigbang(mountain, lis2mountain, n);
-            break;
-        case 200:
-            int h;
-            cin >> h;
-            build_mountain(mountain, lis2mountain, h);
-            break;
-        case 300:
-            earthquake(mountain, lis2mountain);
-            break;
-        case 400:
-            int idx;
-            cin >> idx;
-            cout << simulation(mountain, lis2mountain, idx-1) << endl;
-            break;
-        default:
-            cout << "cmd error" << endl;
-            return 0;
+        }
+        if(o==200){
+            int h;cin>>h;
+            int idx=search(h);
+            index.push_back(idx);
+            if(idx==mount.size())
+                mount.push_back({});
+            mount[idx].push_back(h);
+        }
+        if(o==300){
+            int idx=index.back();
+            index.pop_back();
+            mount[idx].pop_back();
+            if(mount.back().size()==0)
+                mount.pop_back();
+        }
+        if(o==400){
+            int m_idx;cin>>m_idx;
+            m_idx-=1;
+            cout<<(index[m_idx]+mount.size())*1000000+mount.back()[0]<<"\n";
         }
     }
 
